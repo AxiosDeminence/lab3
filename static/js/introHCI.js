b451bafe225895b5d69dea251c62bd7cbc5a4185
Juhmer Tena <juhmer@att.net>
2021-01-21 01:14:28 -0800

    Complete lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,7 +3,22 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$("a.thumbnail").click(projectClick);
 })
+
+function projectClick(e) {
+    e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+
+	var containingProject = $(this).closest(".project");
+	var description = containingProject.find(".project-description");
+	if (description.length == 0) {
+		containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		description.toggle();
+		// description.html(`<p>Stop clicking on me! You just did it at ${new Date()}</p>`);
+	}
+}
 
 /*
  * Function that is called when the document is ready.
@@ -11,6 +26,9 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+
+		$("#testjs").text("Wax on, wax off");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
